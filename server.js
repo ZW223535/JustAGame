21964297bd9be7505f8d44cd4f1b7d6ac783e13c
ZW223535 <zeya.wu@mail.polimi.it>
2022-11-23 11:10:00 +0100

    Commit

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("hello hello");
+console.log("script loaded");
+
+//Use express module - we are loading the express
+let express = require("express");
+
+//Create a web application - we are activatin the express
+
+let app = express();
+
+//Define a port number used to communicate betwen the srver and the host
+
+let port = 3000; //we can't use the same port number for more than 1 service
+
+let server = app.listen(port);
+
+app.use(express.static("public"));
+
+console.log("running server on http://localhost:" + port);
